85b5600e8310577cea7f3254f7da9ce20e726e6e
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-01-25 14:38:48 +0200

    Added page

--- e-commerce-app/app/page.tsx
+++ e-commerce-app/app/page.tsx
@@ -1,5 +1,30 @@
+import { truncateText } from "@/utils/truncate";
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBaner";
+import { products } from "@/utils/products"
+
+
 export default function Home() {
   return (
-    <main>E-shop</main>
+    <div className="p-8">
+      <Container>
+        <div><HomeBanner/></div>
+        <div className="
+        grid 
+        grid-cols-2 
+        sm:grid-cols-3 
+        lg:grid-cols-4 
+        xl:grid-cols-5
+        2xl:grid-col-6 
+        gap:8
+        ">
+          {products.map(
+            (product: any) => {
+              return <div>{truncateText(product.name)}</div>
+            }
+          )}
+        </div>  
+      </Container>
+    </div>
   )
 }
