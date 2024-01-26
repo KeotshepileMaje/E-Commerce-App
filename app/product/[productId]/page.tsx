1e29c2b0a80fe89fcb7f07d92716c4dfd5ba019a
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-01-26 20:15:43 +0200

    added product details

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,11 +1,20 @@
+import Container from "@/app/components/Container";
+import { product } from "@/utils/product";
+import ProductDetails from "./ProductDetails";
+
 interface IParams {
     productId?: string;
 }
 
 const Product = ({params}: {params: IParams}) => {
     console.log('param,,, ',params)
+    
     return ( 
-        <div>Product Page</div>
+        <div className="p-8">
+            <Container>
+                <ProductDetails product={product}/>
+            </Container>
+        </div>
     );
 }
 
