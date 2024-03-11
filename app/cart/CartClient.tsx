d9b8311d007b91e317c04846b238be9a3345c248
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-03-11 08:23:06 +0200

    introduced user for conditional rendering

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { useCart } from "../hooks/useCart";
@@ -7,45 +7,51 @@ import Heading from "../components/Heading";
 import Button from "../components/Button";
 import ItemContent from "./ItemContent";
 import { formatPrice } from "@/utils/formatPrice";
+import { SafeUser } from "@/types";
+import { useRouter } from "next/navigation";
 
-const CartClient = () => {
+interface CartClientProps {
+  currentUser: SafeUser | null;
+}
 
-    const { 
-        cartProducts,
-        handleClearCart,
-        cartTotalAmount,
-    } = useCart()
+const CartClient: React.FC<CartClientProps> = ({ currentUser }) => {
+  const { cartProducts, handleClearCart, cartTotalAmount } = useCart();
+  const router = useRouter();
 
-    if (!cartProducts || cartProducts.length === 0) {
-        return (
-            <div className="
+  if (!cartProducts || cartProducts.length === 0) {
+    return (
+      <div
+        className="
             flex
             flex-col
             items-center
-            ">
-                <div className="text-2xl">Your cart is empty</div>
-                <div>
-                    <Link href={'/'}
-                        className="
+            "
+      >
+        <div className="text-2xl">Your cart is empty</div>
+        <div>
+          <Link
+            href={"/"}
+            className="
                         text-slate-500
                         flex 
                         items-center 
                         gap-1 
                         mt-2
-                    ">
-                        <MdArrowBack />
-                        <span>Start Shopping</span>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+                    "
+          >
+            <MdArrowBack />
+            <span>Start Shopping</span>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 
-
-    return ( 
-        <div>
-            <Heading title='Shopping Cart' center />
-            <div className="
+  return (
+    <div>
+      <Heading title="Shopping Cart" center />
+      <div
+        className="
             grid
             grid-cols-5
             text-xs
@@ -53,78 +59,86 @@ const CartClient = () => {
             pb-2
             items-center
             mt-8
-            ">
-                <div className="col-span-2 justify-self-start">PRODUCT</div>
-                <div className="justify-self-center">PRICE</div>
-                <div className="justify-self-center">QUANTITY</div>
-                <div className="justify-self-end">TOTAL</div>
-            </div>
-            <div>
-                {
-                    cartProducts && cartProducts.map((item) => {
-                        return (
-                            <ItemContent key={item.id} item={item} />
-                        )
-                    }
-                    )
-                }
-            </div>
-            
-            <div className="
+            "
+      >
+        <div className="col-span-2 justify-self-start">PRODUCT</div>
+        <div className="justify-self-center">PRICE</div>
+        <div className="justify-self-center">QUANTITY</div>
+        <div className="justify-self-end">TOTAL</div>
+      </div>
+      <div>
+        {cartProducts &&
+          cartProducts.map((item) => {
+            return <ItemContent key={item.id} item={item} />;
+          })}
+      </div>
+
+      <div
+        className="
             border-t-[1.5px] 
             border-slate-200
             py-4
             flex
             justify-between
             gap-4
-            ">
-                <div className="w-[100px]">
-                    <Button 
-                        label='Clear Cart' 
-                        onClick={() => {handleClearCart()}}
-                        small
-                        outline
-                    />
-                </div>
-                <div className="
+            "
+      >
+        <div className="w-[100px]">
+          <Button
+            label="Clear Cart"
+            onClick={() => {
+              handleClearCart();
+            }}
+            small
+            outline
+          />
+        </div>
+        <div
+          className="
                 text-sm 
                 flex 
                 flex-col 
                 gap-1 
                 items-start
-                ">
-                    <div className="
+                "
+        >
+          <div
+            className="
                     flex 
                     justify-between
                     w-full
                     text-base
                     font-semibold
-                    ">
-                        <span>Subtotal</span>
-                        <span>{formatPrice(cartTotalAmount)}</span>
-                    </div>
-                    <p className="text-slate-500">Taxes and and shipping calculate at checkout</p>
-                    <Button 
-                        label='Checkout' 
-                        onClick={() => {}}
-                    />
-                    <Link
-                        href={'/'}
-                        className="
+                    "
+          >
+            <span>Subtotal</span>
+            <span>{formatPrice(cartTotalAmount)}</span>
+          </div>
+          <p className="text-slate-500">
+            Taxes and and shipping calculate at checkout
+          </p>
+          <Button
+            label={currentUser ? "CheckOut" : "Login to CheckOut"}
+            outline={currentUser ? false : true}
+            onClick={() => {currentUser ? router.push('/checkout') : router.push('/login')}}
+          />
+          <Link
+            href={"/"}
+            className="
                         text-slate-500
                         flex
                         items-center
                         gap-1
                         mt-2
                         "
-                    >
-                        <MdArrowBack />
-                        <span>Continue Shopping</span>
-                    </Link>
-                </div>
-            </div>
+          >
+            <MdArrowBack />
+            <span>Continue Shopping</span>
+          </Link>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default CartClient;
